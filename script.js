a68a6c632fd44936e6e2e9eba17759c669828a3e
10CY <109400270+10CY@users.noreply.github.com>
2022-09-17 12:58:40 +0530

    this is responsive

---
+++ script.js
@@ -0,0 +1,6 @@
+const mobile_nav =document.querySelector(".mobile-navbar-icon");
+const nav_header = document.querySelector(".header");
+const toggleNavBar=()=>{
+    nav_header.classList.toggle("active");
+}
+mobile_nav.addEventListener("click",()=>toggleNavBar());
